19f6c928c541995df94478ad6bc21f8908a202bf
moslem <moslemamiri82@gmail.com>
2025-08-30 10:00:54 +0330

    changed urls.py

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contact/', views.home, name='contact'),
-    path('about/', views.home, name='about'),
-    
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+
 ]
