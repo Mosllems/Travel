2232d7a4bd189dd3f86334775f5bd8810df345c4
moslem <moslemamiri82@gmail.com>
2025-09-02 15:45:07 +0330

    add app_name for addressing the pages

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'website' # we must write the apps'name here, because once the project get bigger calling the urls will be harder, and by app name we can distinguish them from eachother
+
 
 urlpatterns = [
     path('', views.home, name='home'),
