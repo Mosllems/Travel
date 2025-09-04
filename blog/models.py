9f66a9ff42d7a3eb517f0a06ad3bc97bb18e6bc2
moslem <moslemamiri82@gmail.com>
2025-09-04 20:43:27 +0330

    add author for blogs

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
+    author = models.CharField(max_length=200)
+
     
