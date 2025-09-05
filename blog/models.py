7c871037210111d55292d27b28b47c397fc6412e
moslem <moslemamiri82@gmail.com>
2025-09-05 14:00:36 +0330

    add contact model and modified post model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
-    author = models.CharField(max_length=200)
-
+    # image
+    # author 
+    # tag
+    # category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
     
