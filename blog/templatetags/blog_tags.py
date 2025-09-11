be32c8c43581e4b942d2493ea40c67d908832f6f
moslem <moslemamiri82@gmail.com>
2025-09-11 23:59:48 +0330

    implement getting the latest blogs in single-blog

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,7 +4,7 @@ from blog.models import Post
 register = template.Library()
 
 
-@register.inclusion_tag('blog/templates/blog/blog-latest_blogs.html')
+@register.inclusion_tag('blog/blog-latest_blogs.html')
 def get_latest_blogs(count=3):
-    posts = Post.objects.filter('created_date')[:count]
+    posts = Post.objects.filter(status=1)[:count]
     return {'posts': posts}
