ead106f1ca15b47a026d185a71f0b93fcfbcf271
moslem <moslemamiri82@gmail.com>
2025-09-13 21:48:52 +0330

    implement filtering according to category

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from blog.models import Post, Category
 
 
 def blogview(request):
-    posts = Post.objects.filter(status= 1)
+    posts = Post.objects.filter(status=1).order_by("-created_date")
+    category = request.GET.get("category")
+    if category:
+        posts = Post.objects.filter(status=1, category__name=category).order_by("-created_date")
     return render (request, "blog/blog-home.html", context= {
         'posts': posts,
-
+        
         })
 
 
