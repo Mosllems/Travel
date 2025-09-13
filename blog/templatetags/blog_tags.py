eb0f98185fd830917209bd386c86816c818d9a21
moslem <moslemamiri82@gmail.com>
2025-09-13 20:57:23 +0330

    add category in blog's pages

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -8,3 +8,9 @@ register = template.Library()
 def get_latest_blogs(count=3):
     posts = Post.objects.filter(status=1)[:count]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-category.html')
+def get_category():
+    categories = Category.objects.all().order_by('name')
+    return {'categories': categories}
