6b65db1c47bf36725751cd377344f747ec85f61d
moslem <moslemamiri82@gmail.com>
2025-09-16 18:39:20 +0330

    add messages in contact page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -16,7 +17,9 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-    form = ContactForm()
+            messages.success(request, "Your message has been added successfully")
+    else:
+        form = ContactForm()
 
     return render(request, 'contact.html', {
         'form': form,
